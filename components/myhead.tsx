3dd9606945a0b5fec9767bd34bc5a4a37b572bfe
unknown <freelancestanislav@gmail.com>
2023-02-20 00:36:05 +0300

    add meta description

--- components/myhead.tsx
+++ components/myhead.tsx
@@ -3,6 +3,7 @@ import { Head } from "$fresh/runtime.ts";
 export default function MyHead() {
   return (
     <Head>
+      <meta name="description" content="Новый взгляд на старю вещь" />
       <title>Legist fresh</title>
     </Head>
   );
