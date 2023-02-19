a44c69265673633ceb69a0bac8c25ed492b86550
unknown <freelancestanislav@gmail.com>
2023-02-20 00:42:17 +0300

    change meta description; add meta tags; add meta author

--- components/myhead.tsx
+++ components/myhead.tsx
@@ -3,7 +3,9 @@ import { Head } from "$fresh/runtime.ts";
 export default function MyHead() {
   return (
     <Head>
-      <meta name="description" content="Новый взгляд на старю вещь" />
+      <meta name="description" content="Профессиональная защита ваших интересов от опытных адвокатов" />
+      <meta name="keywords" content="защита, адвокаты, минск, помощь, проблема, профессиональная">
+      <meta name="author" content="Stansilav Smirnov">
       <title>Legist fresh</title>
     </Head>
   );
